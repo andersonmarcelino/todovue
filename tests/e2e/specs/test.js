f30a2e2eaf01967f733560ba7c4dbd3285d7f09a
Anderson Marcelino <anderson44444@gmail.com>
2020-02-06 15:26:27 +0000

    specs for check item

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,15 +1,36 @@
 // https://docs.cypress.io/api/introduction/api.html
 
+function setup (cy) {
+  cy.visit('/')
+  cy.get('[data-test="input-add"]').type('Criar Lista{enter}')
+  cy.get('[data-test="input-add"]').type('Executar a Lista{enter}')
+  cy.get('[data-test="input-add"]').type('Finalizar a Lista{enter}')
+}
+
 describe('Todolist', () => {
-  it('Adds a item on todolist', () => {
-    cy.visit('/')
-    cy.get('[data-test="input-add"]').type('Primeira Tarefa{enter}')
-    cy.get('[data-test="todo-list"]').contains('Primeira Tarefa')
+  describe('Add Item', () => {
+    it('Adds a item on todolist', () => {
+      cy.visit('/')
+      cy.get('[data-test="input-add"]').type('Primeira Tarefa{enter}')
+      cy.get('[data-test="todo-list"]').contains('Primeira Tarefa')
+    })
+
+    it('Clear the input', () => {
+      cy.visit('/')
+      cy.get('[data-test="input-add"]').type('Primeira Tarefa{enter}')
+      cy.get('[data-test="input-add"]').should('have.value', '')
+    })
   })
 
-  it('Clear the input', () => {
-    cy.visit('/')
-    cy.get('[data-test="input-add"]').type('Primeira Tarefa{enter}')
-    cy.get('[data-test="input-add"]').should('have.value', '')
+  describe('check item', () => {
+    it('Move item to list below', () => {
+      setup(cy)
+
+      cy.get('[data-test="todo-list"] li:nth-child(2) [type=checkbox]')
+        .click()
+
+      cy.get('[data-test="done-list"] li')
+        .should('have.length', 1)
+    })
   })
 })
