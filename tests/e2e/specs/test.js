75817dc1ee764e7256352e26debb4a189dbec9e5
Anderson Marcelino <anderson44444@gmail.com>
2020-02-06 00:25:22 +0000

    task 1 only sṕec

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,7 +1,9 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('Todolist', () => {
-  it('Visits the app root url', () => {
+  it('Adds a item on todolist', () => {
     cy.visit('/')
+    cy.get('[data-test="input-add"]').type('Primeira Tarefa{enter}')
+    cy.get('[data-test="todo-list"]').contains('Primeira Tarefa')
   })
 })
