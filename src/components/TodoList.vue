c0e2fc40fe02879deb13dcade3232a705aae89e8
Anderson Marcelino <anderson44444@gmail.com>
2020-02-06 14:54:28 +0000

    Add item on todolist

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,29 @@
 <template>
-  <div>Clear</div>
+  <section class="todo-list">
+    <input data-test="input-add" @keyup.enter="addItem" v-model="inputAdd"/>
+    <ul data-test="todo-list">
+      <li v-for="item in todoList" :key="item.id">
+        {{item.label}}
+      </li>
+    </ul>
+  </section>
 </template>
 <script>
   export default {
-    name: 'TodoList'
+    name: 'TodoList',
+    data () {
+      return {
+        todoList: [],
+        inputAdd: '',
+      }
+    },
+    methods: {
+      addItem () {
+        this.todoList.push({
+          id: Math.random(),
+          label: this.inputAdd
+        })
+      }
+    }
   }
 </script>
